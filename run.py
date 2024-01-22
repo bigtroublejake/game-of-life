55e962fbf3de0c0ad152632ee15d24dccba96d89
bigtroublejake <bigtroublejake@gmail.com>
2024-01-22 18:55:16 -0500

    Added .gitignore
    also added comment to run file

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 from game_of_life_funcs import random_state, eternal_life
 
+
+"""
+THIS SHOULD BE RUN IN THE TERMINAL
+
+THERE IS A GOOD CHANCE IT WILL NOT WORK 
+IF EVERYTHING IS OUTPUTTED IN AN 'OUTPUT' TAB
+"""
+
+
 w = int(input("width: "))
 h = int(input("height: "))
 
